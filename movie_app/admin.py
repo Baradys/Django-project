87c4b9ecbb5fd2ea289ff94e74f84c345e49c790
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-04 20:25:02 +0300

    Search bar added.

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -10,6 +10,7 @@ class MovieAdmin(admin.ModelAdmin):
     ordering = ['-rating', 'name']
     list_per_page = 10
     actions = ['set_dollars', 'set_rubles', 'set_euros']
+    search_fields = ['name__startswith']
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, movie: Movie):
