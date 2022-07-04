15b06dbbdda56b5d3a10e4965ccf2eeea0bc718d
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-04 20:09:04 +0300

    Currency actions added.

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,16 +1,15 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
-
-
-# Register your models here.
+from django.db.models import QuerySet
 
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget', 'currency', 'rating_status']
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status', 'currency']
     list_editable = ['rating', 'year', 'budget', 'currency']
     ordering = ['-rating', 'name']
     list_per_page = 10
+    actions = ['set_dollars', 'set_rubles', 'set_euros']
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, movie: Movie):
@@ -21,3 +20,28 @@ class MovieAdmin(admin.ModelAdmin):
         if movie.rating <= 85:
             return 'Зачет'
         return 'Топчик'
+
+    @admin.display(description='Установить валюту в доллар')
+    def set_dollars(self, request, qs: QuerySet):
+        count_updates = qs.update(currency=Movie.USD)
+        self.message_user(
+            request,
+            f'Было обновлено {count_updates} записей'
+        )
+
+    @admin.display(description='Установить валюту в рубль')
+    def set_rubles(self, request, qs: QuerySet):
+        count_updates = qs.update(currency=Movie.USD)
+        self.message_user(
+            request,
+            f'Было обновлено {count_updates} записей'
+        )
+
+    @admin.display(description='Установить валюту в евро')
+    def set_euros(self, request, qs: QuerySet):
+        count_updates = qs.update(currency=Movie.USD)
+        self.message_user(
+            request,
+            f'Было обновлено {count_updates} записей',
+            messages.ERROR
+        )
