88286d8383e8877b37f3bcc9d1711e033733aa19
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-14 16:13:01 +0300

    Using classes instead functions

--- feedback/views.py
+++ feedback/views.py
@@ -1,18 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
+from .models import Feedback
+from django.views import View
 
 
-def index(request):
-    if request.method == 'POST':
+class FeedBackView(View):
+    def get(self, request):
+        form = FeedbackForm()
+        return render(request, 'feedback/feedback.html', context={'form': form})  # Приходим на пустую ветку
+
+    def post(self, request):
         form = FeedbackForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('done/')
-    else:
-        form = FeedbackForm()
-    return render(request, 'feedback/feedback.html', context={'form': form})  # Приходим на пустую ветку
+        return render(request, 'feedback/feedback.html', context={'form': form})  # Приходим на пустую ветку
 
 
-def done(request):
-    return render(request, 'feedback/done.html')
+class FeedBackUpdateView(View):
+    def get(self, request, id_feedback):
+        feed = Feedback.objects.get(id=id_feedback)
+        form = FeedbackForm(instance=feed)
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+    def post(self, request, id_feedback):
+        feed = Feedback.objects.get(id=id_feedback)
+        form = FeedbackForm(request.POST, instance=feed)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(f'{id_feedback}')
+        return render(request, 'feedback/feedback.html', context={'form': form})
+
+
+class DoneView(View):
+    def get(self, request):
+        return render(request, 'feedback/done.html')
