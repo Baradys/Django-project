51f457ad233926dd0a7c4541f03133e33df9033d
Mikhail Bratukhin <bym97m@mail.ru>
2022-07-14 13:50:16 +0300

    database and css added

--- feedback/forms.py
+++ feedback/forms.py
@@ -2,6 +2,11 @@ from django import forms
 
 
 class FeedbackForm(forms.Form):
-    name = forms.CharField()
-    surname = forms.CharField()
-    feedback = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols': 20}))
+    name = forms.CharField(label='Имя', max_length=10, min_length=2, error_messages={
+        'max_length': 'Слишком много символов',
+        'min_length': 'Слишком мало символов',
+        'required': 'Укажите хотя бы 1 символ'
+    })
+    surname = forms.CharField(label='Фамилия')
+    feedback = forms.CharField(label='Отзыв', widget=forms.Textarea(attrs={'rows': 2, 'cols': 20}))
+    rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1)
--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
+from .models import Feedback
 
 
 def index(request):
@@ -8,8 +9,16 @@ def index(request):
         form = FeedbackForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
+            feed = Feedback(
+                name=form.cleaned_data['name'],
+                surname=form.cleaned_data['surname'],
+                feedback=form.cleaned_data['feedback'],
+                rating=form.cleaned_data['rating'],
+            )
+            feed.save()
             return HttpResponseRedirect('done/')
-    form = FeedbackForm()
+    else:
+        form = FeedbackForm()
     return render(request, 'feedback/feedback.html', context={'form': form})  # Приходим на пустую ветку
 
 
